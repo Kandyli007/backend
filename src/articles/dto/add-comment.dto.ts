af698af195dcfbb8076f953fe61b7d00868312e4
CJJ <474477759@qq.com>
2025-05-16 17:04:08 +0800

    🎉 后端 ready for deployment

---
+++ src/articles/dto/add-comment.dto.ts
@@ -0,0 +1,9 @@
+import { IsString } from 'class-validator';
+
+export class AddCommentDto {
+  @IsString()
+  author: string;
+
+  @IsString()
+  content: string;
+}
---
+++ src/articles/dto/create-article.dto.ts
@@ -0,0 +1,14 @@
+import { IsString, IsOptional } from 'class-validator';
+
+export class CreateArticleDto {
+  @IsString()
+  title: string;
+
+  @IsString()
+  @IsOptional()
+  authors?: string;
+
+  @IsString()
+  @IsOptional()
+  abstract?: string;
+}
